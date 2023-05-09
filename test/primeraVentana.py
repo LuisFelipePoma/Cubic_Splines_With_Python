2afb889ef934de0950911b70fa6b100cd97ff261
Felipe <u202110902@upc.edu.pe>
2023-05-09 18:42:20 -0500

    Testeo de ventanas en Tkinter y frames de la GUI

--- test/primeraVentana.py
+++ test/primeraVentana.py
@@ -1,18 +1,61 @@
-import tkinter as tk
+import random
+from tkinter import *
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from scipy.interpolate import CubicSpline
 
-# Creamos el objeto de la clase
+# Función para calcular los polinomios cúbicos a partir de los puntos
+def calculate_polynomials(points):
+    xs, ys = zip(*points)
+    points = sorted(zip(xs, ys))
+    xs, ys = zip(*points)
+    cs = CubicSpline(xs, ys)
+    return xs, ys, cs
 
-ventana = tk.Tk()
+# Función para generar puntos aleatorios
+def generate_points(n):
+    points = [(random.uniform(0, 1), random.uniform(0, 1)) for i in range(n)]
+    return points
 
-# Dimensiones
+# Función para graficar los polinomios
+def plot_polynomials(xs, ys, cs, ax):
+    ax.clear()
+    ax.scatter(xs, ys)
+    xnew = np.linspace(min(xs), max(xs), 1000)
+    ynew = cs(xnew)
+    ax.plot(xnew, ynew)
 
-ancho = 800
-alto = 600
-tamanio = str(ancho) + 'x' + str(alto)
-ventana.geometry(tamanio)
+# Función para manejar el botón "Generar"
+def handle_generate():
+    n = int(entry_n.get())
+    points = generate_points(n)
+    xs, ys, cs = calculate_polynomials(points)
+    plot_polynomials(xs, ys, cs, ax)
 
-# Titulo
-ventana.title('Titulo de la ventana')
+# Configuración de la ventana principal
+root = Tk()
+root.geometry("600x800")
 
-# Poner siempre al final
-ventana.mainloop()
+# Crear un marco para contener la figura
+frame = Frame(root)
+frame.pack(side=TOP, fill=BOTH, expand=True)
+
+# Crear una etiqueta y una entrada para el valor de n
+label_n = Label(root, text="Valor de n:")
+label_n.pack(in_=frame, side=LEFT)
+entry_n = Entry(root)
+entry_n.pack(in_=frame, side=LEFT)
+
+# Crear un botón y vincularlo a la función handle_generate
+generate_button = Button(root, text="Generar", command=handle_generate)
+generate_button.pack(in_=frame, side=LEFT)
+
+# Crear la figura de matplotlib y agregarla al marco
+fig = plt.Figure()
+ax = fig.add_subplot(111)
+canvas = FigureCanvasTkAgg(fig, master=root)
+canvas.get_tk_widget().pack(side=BOTTOM, fill=BOTH, expand=True)
+
+# Iniciar el bucle de eventos principal de tkinter
+root.mainloop()
